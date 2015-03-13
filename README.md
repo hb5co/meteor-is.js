d92115e71d323a26f79a2ff8382f46158827e542
Mark Shropshire <mdshrops@shropnet.net>
2015-03-13 12:26:35 -0400

    Updated readme and added version number and git repo to package info

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# is.js for Meteor
+# is.js micro check library for Meteor.
+
+"Check types, regexps, presence, time and more..."
+See: http://arasatasaygin.github.io/is.js/
--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 Package.describe({
-  summary: "is.js for Meteor"
+  summary: "is.js micro check library for Meteor",
+  version: "0.0.1",
+  git: "https://github.com/hb5co/meteor-is.js.git" 
 });
 
 Package.on_use(function (api, where) {
