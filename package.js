9e91addce523b4e7a517309088feb86cd55330ee
Mark Shropshire <mdshrops@shropnet.net>
2015-03-13 12:52:12 -0400

    Updated package description info

--- package.js
+++ package.js
@@ -1,9 +1,10 @@
 Package.describe({
-  summary: "is.js micro check library for Meteor",
-  version: "0.0.1",
-  git: "https://github.com/hb5co/meteor-is.js.git" 
+  name: 'hb5:meteor-is.js',
+  summary: 'is.js micro check library for Meteor',
+  version: '0.0.1',
+  git: 'https://github.com/hb5co/meteor-is.js.git'
 });
 
-Package.on_use(function (api, where) {
+Package.on_use(function(api) {
   api.add_files('lib/is.js/is.min.js');
 });
